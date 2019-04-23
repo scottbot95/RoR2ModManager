9a41a686e7c9ee6664a2a414c2c6954efa987221
Scott Techau <scotttechau@gmail.com>
2019-04-23 09:57:13 -0700

    refactor: Change prefs replacment to support web

---
+++ src/electron/prefs.web.ts
@@ -0,0 +1,26 @@
+type cb = (newValue: any, oldValue: any) => void;
+
+class WebPrefsStore {
+  private watchers: { [key: string]: cb[] } = {};
+
+  get(key: string) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+  set(key: string, value: any) {
+    const keyWatchers = this.watchers[key];
+    if (Array.isArray(keyWatchers) && keyWatchers.length > 0) {
+      const oldValue = this.get(key);
+      keyWatchers.forEach(watcher => watcher(value, oldValue));
+    }
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+  onDidChange(key: string, cb: cb) {
+    if (!Array.isArray(this.watchers[key])) {
+      this.watchers[key] = [];
+    }
+
+    this.watchers[key].push(cb);
+  }
+}
+
+export const prefs = new WebPrefsStore();
