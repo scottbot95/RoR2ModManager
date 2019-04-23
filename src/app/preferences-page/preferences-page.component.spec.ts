8cff2ce1d46d86563621efc288cebae8acf8631b
Scott Techau <scotttechau@gmail.com>
2019-04-23 11:21:37 -0700

    feat: Wire up dark mode setting

--- src/app/preferences-page/preferences-page.component.spec.ts
+++ src/app/preferences-page/preferences-page.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PreferencesPageComponent } from './preferences-page.component';
+import { PreferencesService } from '../core/services/preferences.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+
+class MockPrefsService {}
 
 describe('PreferencesPageComponent', () => {
   let component: PreferencesPageComponent;
@@ -8,9 +13,10 @@ describe('PreferencesPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PreferencesPageComponent ]
-    })
-    .compileComponents();
+      declarations: [PreferencesPageComponent],
+      imports: [SharedModule, ReactiveFormsModule],
+      providers: [{ provide: PreferencesService, useClass: MockPrefsService }]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/preferences-page/preferences-page.component.ts
+++ src/app/preferences-page/preferences-page.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PreferencesService } from '../core/services/preferences.service';
+import { FormBuilder } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-preferences-page',
   templateUrl: './preferences-page.component.html',
   styleUrls: ['./preferences-page.component.scss']
 })
-export class PreferencesPageComponent implements OnInit {
+export class PreferencesPageComponent implements OnInit, OnDestroy {
+  constructor(private prefs: PreferencesService, private fb: FormBuilder) {}
 
-  constructor() { }
+  darkMode = this.fb.control(this.prefs.get('darkMode'));
+
+  private subscription = new Subscription();
 
   ngOnInit() {
+    this.subscription.add(
+      this.prefs.onChange('darkMode').subscribe(({ newValue }) => {
+        this.darkMode.setValue(newValue);
+      })
+    );
+
+    this.subscription.add(
+      this.darkMode.valueChanges.subscribe(newValue => {
+        this.prefs.set('darkMode', newValue);
+      })
+    );
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
