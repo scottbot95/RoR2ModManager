4fc415509828382bc2a003f7b5242edcd1df3c71
Scott Techau <scotttechau@gmail.com>
2019-04-22 21:22:09 -0700

    feat: Copy RoR2Dir out of window registry
    Also cleanup the windows registry after copy

--- src/electron/prefs.ts
+++ src/electron/prefs.ts
@@ -1,8 +1,43 @@
 import * as Store from 'electron-store';
+import * as Registry from 'winreg';
 import { defaultConfig, preferencesSchema } from './preferences.model';
+import { productName } from '../../electron-builder.json';
+
+const regKey = new Registry({
+  hive: Registry.HKCU,
+  key: `\\SOFTWARE\\${productName}`
+});
+
+regKey.values((err, items) => {
+  if (err) {
+    console.error('Failed to read registry');
+    console.error(err);
+  } else {
+    items.forEach(item => {
+      console.log(`ITEM:${item.name}\t${item.type}\t${item.value}`);
+    });
+  }
+});
 
 export const prefs = new Store({
   name: 'user-preferences',
   defaults: defaultConfig,
   schema: preferencesSchema
+});
+
+// Grab value out of registry
+regKey.get('RoR2Dir', (err, result) => {
+  if (!err) {
+    // save it to prefs
+    prefs.set('ror2_path', result.value);
+
+    // remove it from registry as we'll use the pref from here on out
+    regKey.destroy(err => {
+      if (err) {
+        console.error(
+          `Failed to remove registry key ${regKey.hive}${regKey.key}`
+        );
+      }
+    });
+  }
 });
