4a7e0177b415fd488e9649d558afcd48edb543fa
Scott Techau <scotttechau@gmail.com>
2019-04-23 19:26:35 -0700

    refactor: Remove HomeComponent

--- e2e/main.spec.ts
+++ e2e/main.spec.ts
@@ -1,28 +1,26 @@
-import {expect, assert} from 'chai';
-import {SpectronClient} from 'spectron';
+import { expect, assert } from 'chai';
+import { SpectronClient } from 'spectron';
 
 import commonSetup from './common-setup';
 
-describe('ror2-mod-manager App', function () {
+describe('ror2-mod-manager App', function() {
   commonSetup.apply(this);
 
   let browser: any;
   let client: SpectronClient;
 
-  beforeEach(function () {
+  beforeEach(function() {
     client = this.app.client;
     browser = client as any;
   });
 
-  it('should display message saying App works !', async function () {
+  xit('should display message saying App works !', async function() {
     const text = await browser.getText('app-home h1');
     expect(text).to.equal('App works !');
   });
 
-
-  it('creates initial windows', async function () {
+  it('creates initial windows', async function() {
     const count = await client.getWindowCount();
     expect(count).to.equal(1);
   });
-
 });
