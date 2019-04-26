eb1d3e2b24780063afb79f3906f1f841de632c92
Scott Techau <scotttechau@gmail.com>
2019-04-26 12:43:00 -0700

    fix: Only show dev tools in development

--- main.ts
+++ main.ts
@@ -101,9 +101,9 @@ function createWindow() {
     );
   }
 
-  // if (serve) {
-  win.webContents.openDevTools();
-  // }
+  if (serve) {
+    win.webContents.openDevTools();
+  }
 
   // Emitted when the window is closed.
   win.on('closed', () => {
