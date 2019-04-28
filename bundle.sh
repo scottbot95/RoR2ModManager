d92c05def9b63b8cdc5fcc7933b996debb5d8491
scottbot95 <scotttechau@gmail.com>
2019-04-28 14:14:12 -0700

    Travis fixes (#28)
    fix travis and update readme

--- bundle.sh
+++ bundle.sh
@@ -7,14 +7,23 @@ PRODUCT_NAME=$(jq -r '.productName' electron-builder.json)
 ZIP_FILE="$PACKAGE_NAME-$PACKAGE_VERSION.zip"
 RELEASE_FILE="$PRODUCT_NAME Setup $PACKAGE_VERSION.exe"
 
+echo UNPACKED_DIR=$UNPACKED_DIR
+echo PACKAGE_NAME=$PACKAGE_NAME
+echo PACKAGE_VERSION=$PACKAGE_VERSION
+echo PRODUCT_NAME=$PRODUCT_NAME
+echo ZIP_FILE=$ZIP_FILE
+echo RELEASE_FILE=$RELEASE_FILE
+
+
+
 # Create unpacked dir
 mkdir -p "$UNPACKED_DIR"
 # create manifest.json
 jq '{name:"RoR2ModManager", version_number:.version, website_url:.homepage, description:.description, dependencies:[]}' package.json > "$UNPACKED_DIR/manifest.json"
 cd $UNPACKED_DIR
 # link rest of files
-rm "$RELEASE_FILE" && ln "../$RELEASE_FILE"
-rm icon.png && ln "../../src/favicon.256x256.png" icon.png
+rm "$RELEASE_FILE"; ln "../$RELEASE_FILE"
+rm icon.png; ln "../../src/favicon.256x256.png" icon.png
 # link readme if it doesn't exist already
 if [ ! -f README.md ]; then
   ln ../../README.md
