4af8956cac9f691b65559c8df2911cebb32365fe
Scott Techau <scottbot95@gmail.com>
2019-04-28 12:14:34 -0700

    feat: Create bundle script

---
+++ bundle.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+UNPACKED_DIR="./release/thunderstore-unpacked"
+PACKAGE_NAME=$(jq -r '.name' package.json)
+PACKAGE_VERSION=$(jq -r '.version' package.json)
+PRODUCT_NAME=$(jq -r '.productName' electron-builder.json)
+ZIP_FILE="$PACKAGE_NAME-$PACKAGE_VERSION.zip"
+RELEASE_FILE="$PRODUCT_NAME Setup $PACKAGE_VERSION.exe"
+
+# Create unpacked dir
+mkdir -p "$UNPACKED_DIR"
+# create manifest.json
+jq '{name:"RoR2ModManager", version_number:.version, website_url:.homepage, description:.description, dependencies:[]}' package.json > "$UNPACKED_DIR/manifest.json"
+cd $UNPACKED_DIR
+# link rest of files
+ln ../$RELEASE_FILE
+ln ../src/favicon.256x256.png icon.png
+# link readme if it doesn't exist already
+if [! -f README.md]; then
+  ln ../README.md
+fi
+zip -r $ZIP_FILE .
+
+mv $ZIP_FILE ..
