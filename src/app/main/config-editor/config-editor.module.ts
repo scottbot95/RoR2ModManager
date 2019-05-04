e5772d82d5bbe212a56d3d8122c04b2446b7b800
Scott Techau <scotttechau@gmail.com>
2019-05-04 12:50:55 -0700

    fix: Touch up skeleton slightly to make it more extensible

--- src/app/main/config-editor/config-editor.module.ts
+++ src/app/main/config-editor/config-editor.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ConfigEditorRoutingModule } from './config-editor-routing.module';
 import { FoundConfigTableComponent } from './found-config-table/found-config-table.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [FoundConfigTableComponent],
-  imports: [
-    CommonModule,
-    ConfigEditorRoutingModule
-  ]
+  imports: [SharedModule, ConfigEditorRoutingModule]
 })
-export class ConfigEditorModule { }
+export class ConfigEditorModule {}
--- src/app/main/config-editor/found-config-table/found-config-table.component.ts
+++ src/app/main/config-editor/found-config-table/found-config-table.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { PreferencesService } from '../../../core/services/preferences.service';
+import { ElectronService } from '../../../core/services/electron.service';
+import { Package } from '../../../core/models/package.model';
+
+interface ConfigFile {
+  pkg?: Package;
+  filename: string;
+}
 
 @Component({
   selector: 'app-found-config-table',
@@ -6,10 +14,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./found-config-table.component.scss']
 })
 export class FoundConfigTableComponent implements OnInit {
+  configFiles: ConfigFile[];
 
-  constructor() { }
+  constructor(
+    private prefs: PreferencesService,
+    private electron: ElectronService
+  ) {}
 
   ngOnInit() {
+    this.electron.glob(
+      `${this.prefs.get('ror2_path')}/BepInEx/config/*.cfg`,
+      (err, matches) => {
+        this.configFiles = matches.map(file => ({
+          filename: this.electron.path.basename(file)
+        }));
+      }
+    );
   }
-
 }
