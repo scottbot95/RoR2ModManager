50c5899548e0e4d204a42ae0e16cdf7fe84cc662
Scott Techau <scottbot95@gmail.com>
2019-05-21 15:06:22 -0700

    feat: Create dialog service

--- src/app/dialogs/dialogs.module.ts
+++ src/app/dialogs/dialogs.module.ts
@@ -4,9 +4,12 @@ import { DialogsRoutingModule } from './dialogs-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { MatDialogModule } from '@angular/material';
 import { NewProfileDialogComponent } from './new-profile-dialog/new-profile-dialog.component';
+import { DialogService } from './services/dialog.service';
+import { RenameProfileDialogComponent } from './rename-profile-dialog/rename-profile-dialog.component';
 
 @NgModule({
-  declarations: [NewProfileDialogComponent],
-  imports: [SharedModule, DialogsRoutingModule, MatDialogModule]
+  declarations: [NewProfileDialogComponent, RenameProfileDialogComponent],
+  imports: [SharedModule, DialogsRoutingModule, MatDialogModule],
+  providers: [DialogService]
 })
 export class DialogsModule {}
